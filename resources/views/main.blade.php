73c0685041213eb63534d7fd84b931f3ec02bcc0
comtaken <kkk@kk.com>
2022-10-09 22:00:53 +0900

    indexレイアウト変更

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,39 +4,45 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./css/kyotumain.css" rel="stylesheet"> 
+    <link href="./css/index.css" rel="stylesheet"> 
     <title>メインメニュー</title>
 </head>
 <body>
-    <section class="wrapper">
-        <div class="">
-            <a href="{{route('logout')}}" class="btn btn-primary" style="margin:20px;">ログアウト</a>
-        </div>
-        <div class="container">
-            @if ($logflg)
-                <div class="flash_message">
-                    <div class="alert alert-success">{{session('flash_message')}}</div>
-                </div>
-            @php $_SESSION["logflg"] = false; @endphp
-            @endif
-            <div class="content">
-                <div style= "color:red">TODO: main画面タイルアイコン６枚並ぶようにmain専用のcss作成</div>
-            <table>
-                <tr>
-                    <td>
-                        <a href="{{route('index')}}"><img src="./image/tangocho.jpg" alt="" width="150" height="150"></a>
-                    </td>
-                    <br>
-                    <td>
-                        <a href="#"><img src="./image/ryorisyasin.jpg" alt="" width="150" height="150"></a>
-                    </td>
-                    <td>
-                        <a href="{{route('createUser')}}"><img src="./image/sinkiuser.jpg" alt="" width="150" height="150"></a>
-                    </td>
-                </tr>
-            </table>
-            </div>
-        </div>
-    </section>
+	<div class="wrapper">
+		<section>
+			<div class="">
+				<a href="{{route('logout')}}" class="btn btn-primary" style="margin:20px;">ログアウト</a>
+			</div>
+			<div>
+				@if ($logflg)
+					<div class="flash_message">
+						<div class="alert alert-success">{{session('flash_message')}}</div>
+					</div>
+				@php $_SESSION["logflg"] = false; @endphp
+				@endif
+			</div>
+		</section>
+		<hr>
+		<section>
+			<div class="index_image">
+				<img src="./image/index_image.png" alt="">
+			</div>
+		</section>
+		<hr>
+		<section class="index_menu">
+			<div><a href="{{route('index')}}"><img src="./image/tangocho.jpg" alt="" ></a></div>
+			<div><a href="#"><img src="./image/ryorisyasin.jpg" alt="" ></a></div>
+			<div><a href="{{route('createUser')}}"><img src="./image/sinkiuser.jpg" alt="" ></a></div>
+		</section>
+		<hr>
+		<section>
+			<div>その他ITニュースサイト</div>
+			<ul>
+				<li><a href="https://gigazine.net/?fbclid=IwAR2UKH5ECQHzF0LCDCVIWoVA_ovM9UshTPo3kq9QjaICPTejqCCX8GcxQNo">GIGAZIN</a></li>
+				<li><a href="https://atmarkit.itmedia.co.jp/">@IT</a></li>
+			</ul>
+			<div><a href="">お問い合わせ</a></div>
+		</section>
+	</div>
 </body>
 </html>
